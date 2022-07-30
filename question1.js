304b2a67f390b4f7d9481c7adc49e37c16a7b9e5
justicepelteir <justice.douglas@hey.com>
2022-07-29 19:17:54 -0700

    edit solutions

--- question1.js
+++ question1.js
@@ -1,6 +1,5 @@
 // Specialized function --> more functional programming in nature
 turnStringIntoUrlV1 = function(string){
-    // const stringArrayTwin - create array modeled after string
     const stringUrlTwin = string.split(' ').join('%20');
     return stringUrlTwin
 }
@@ -12,10 +11,10 @@ turnStringIntoUrlV2 = function(string){
 }
 
 // recursive solution --> functional programming oriented
-turnStringIntoUrlV3 = function createNewString(string, target, replacement) {
+turnStringIntoUrlV3 = function editString(string, target, replacement) {
     if (string.includes(target) === false){
         return string;
     }
     string = string.split(target).join(replacement);
-    return createNewString(string, target, replacement);
+    return editString(string, target, replacement);
 };
